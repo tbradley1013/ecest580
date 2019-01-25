df42cec8ff2f463824d6c8db5d590250901cb43c
Tyler Bradley <tyler.bradley@phila.gov>
2019-01-25 17:45:02 -0500

    Updated Homework 1 and In class 3

--- ecest580_hw_1.m
+++ ecest580_hw_1.m
@@ -16,26 +16,14 @@ seq_sub = seq(141:147);
 % revgeneticcode)? Write a function that returns the possible sequences. The input to
 % the function should be a string (amino acid sequence) and the output should be the
 % potential nucleotide sequences.
-revcode = revgeneticcode();
 
-temp_len = length(seq_sub);
-possible_seqs = [""];
-len_out = 1;
-for i = 1:temp_len
-    prot = seq_sub(i);
-    
-    prot_seq = revcode.(upper(prot));
-    
-    [x, y] = meshgrid(possible_seqs, prot_seq);
-    pairs = [x(:) y(:)];
-    
-    temp_out = repelem("a", length(pairs));
-    for j = 1:length(pairs)
-      temp_out(j) = pairs(j, 1) + pairs(j, 2);
-    end
-    possible_seqs = temp_out;
-end
+% See the function rev_seq at the bottom of the script
 
+% c)
+% Using the Matlab function from part b), calculate the number of sequences that could
+% give rise to the 7 amino acid sequence found in part a).
+sub_seq_code = rev_seq(seq_sub);
+length(sub_seq_code)
 
 
 function output = rev_seq(seq)
@@ -58,5 +46,5 @@ for i = 1:temp_len
     end
     possible_seqs = temp_out;
 end
-
+output = possible_seqs;
 end
--- ecest580_in_class3.m
+++ ecest580_in_class3.m
@@ -5,42 +5,67 @@ fib(13)
 
 
 % 2. Calculate levensthein distance between two strings
-lev_dist("kitten", "sitting")
+lev_dist('kitten', 'sitting')
 
 
 function output = fib(n)
+  % create a vector of length n for the output
   out_vec = repelem(0, n);
   
+  % define the first "next" value of the sequence
   next = 1;
   
+  % add the first value of the sequence -> 0
   out_vec(1) = 0;
+  % for loop that goes from 2 (as in second element of the output vector)
+  % and first adds the "next" value to the output sequence and then
+  % calculates the new "next" number by adding the current "next" number to
+  % the last element in the out_vec
   for i = 2:n
       out_vec(i) = next;
       next = out_vec(i-1) + next;
   end
+  %return the out_vec
   output = out_vec;
 
 end
 
 function output = lev_dist(a, b)
+% define a and b as characters
+a = char(a);
+b = char(b);
+% calculate the length of each character
 len_a = length(a);
 len_b = length(b);
-my_mat = zeros(0, len_b+1, len_a+1);
+%create a vector of zeros that is len_a+1 x len_b+1 dimensions
+% we add the 1 to each length so that the first row and column will remain
+% zeroes in the calculation
+my_mat = zeros(len_b+1, len_a+1);
 
+% create a nested for loop that first loops over each row and then each
+% column of each row
 for i = 2:len_b+1
     for j = 2:len_a+1
+      % define the cost of a given diagonal move based on whether the
+      % current cell of the matrix corresponds to a match in the two words
       if a(j-1) == b(i-1)
           cost = 0;
       else
           cost = 1;
       end   
-      hor = my_mat(i-1, j) + 1;
-      ver = my_mat(i, j-1) + 1;
+      % calculate the potential values of the current cell by calculating
+      % what the value would be for a horizontal move (hor), a vertical
+      % move (ver), and a diagonal move (diag)
+      ver = my_mat(i-1, j) + 1;
+      hor = my_mat(i, j-1) + 1;
       diag = my_mat(i-1, j-1) + cost;
-          
+       
+      % define the current cell as the minimum of the possible options
       my_mat(i, j) = min([hor, ver, diag]);
+      
     end   
     
 end
-output = my_mat;
+% return the bottom right most cell
+output = my_mat(len_b+1, len_a+1);
 end
