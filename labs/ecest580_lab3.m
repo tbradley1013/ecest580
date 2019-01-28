c5e1aa92c793a3d3ea0f5c16a714e57c9572df4a
Tyler Bradley <tyler.bradley@phila.gov>
2019-01-27 20:12:35 -0500

    did more work on lab 3 - mostly finished

--- labs/ecest580_lab3.m
+++ labs/ecest580_lab3.m
@@ -72,6 +72,7 @@ Seq_300 = fastaread('labs/lab3_files/seq_300.fasta');
 [Data_tblastx_300, RTOE_300] = blastncbi("labs/lab3_files/seq_300.fasta", 'tblastx', 'database', 'nr');
 blast_300 = getblast(Data_tblastx_300);
 
+
 % Answers:
 % 1. The difference between the two blast methods is that blastn compares
 % the sequence to reference gene's nucleotides while tblastx is translating
@@ -90,3 +91,60 @@ end
 full_close_species
 
 % 4. 
+
+
+
+
+%% Lab 3.2.3:
+% Answer the following questions based on the BLAST results in Lab 3.2.2:
+% 1. What are the top hits of this BLAST search?
+%
+top_hits = repelem("a", 10);
+for i = 1:10
+    top_hits(i) = dino_bn.Hits(i).Definition;
+end
+top_hits
+
+% 2. What organism do you think Mark used for his dinoDNA sequence.
+
+% The top hits for the blast results correspond to X.laevis which is a
+% african clawed frog. This match is found in numerous hits in the top ten
+% blast matches
+
+% Now let?s look for Mark?s hidden message. To do this we need to start a new translated blastx search
+% (blastx). This time use the SwissProt protein database (Matlab parameter: ?database?, ?swissprot?).
+% Answer the questions:
+
+[dino_prot_blast, RTOE_dino_prot] = blastncbi("labs/lab3_files/dinoDNA.fasta", 'blastx', 'database', 'swissprot');
+dino_tblast = getblast(dino_prot_blast);
+
+% 3. Are the top hits for the blastx search the same as the ones you saw for blastn? Why might this be
+% the case?
+% No they are not the same, the blastx search results correspond to a
+% transcription factor protein that is found in different animals. The top
+% hits for the tblastx correspond to this protein in different animals.
+% This could be different than the nucleotide blastn results because the
+% blastx method makes the assumption that the DNA being used is from a
+% coding region of the genome that would actually be transcribed and
+% translated into a polypeptide chain of amino acids and that may not be
+% true. This assumption may result in inaccurate blast matches
+
+% 4. What is the hidden message that Mark put in the sequence?
+% This hidden message can be found in the protein sequence of the dino dna
+% and it says "MARK WAS HERE"
+regexp(nt2aa(dino_seq.Sequence), "MARK|WAS|HERE");
+regexp(nt2aa(dino_seq.Sequence), "MARK|WAS|HERE", "match");
+
+
+
+%% Lab 3.3.1:
+% 1. Retrieve Peptide Sequences from NCBI (human: AAD01939; fly: AAQ67266).
+human_pep = getgenpept('AAD01939');
+fly_pep = getgenpept('AAQ67266');
+% 2. Show the Sequence Dot Plot of these two sequences.
+seqdotplot(human_pep, fly_pep);
+title("Sequence Dot Plot");
+
+% 3. Implement global alignment (Needleman-Wunsch Algorithm) to align these two sequences and
+% show the Scoring space and wining path.
+[s_AA,a_AA] = nwalign(human_pep,fly_pep,'Alphabet','AA','showscore', 'true');
